6cf0b8904448f00bec567208c8e0a1989499a754
edriel <edriel10gama10@gmail.com>
2024-05-31 23:41:21 -0300

    calculadora V1 pronta

--- Exer_7_calculadora/Calculadora_V1/JS/main.js
+++ Exer_7_calculadora/Calculadora_V1/JS/main.js
@@ -1,8 +1,58 @@
 function criaCalculadora() {
     return{
         conteiner: document.querySelector('.conteiner'),
+        display: document.querySelector('.display'),
 
-        iniciar() {conteiner.addEvenListiner}
+        iniciar() {
+            this.clicarBotoes();
+            this.apertarEnter();
+            },
 
-    }
+        apertarEnter() {
+            this.display.addEventListener('keyup', (e) => {
+                if(e.keyCode === 13) this.calcular();
+            })
+        },
+
+        clicarBotoes() {
+            this.conteiner.addEventListener('click', (e) => {
+                el = e.target;
+
+                if(el.classList.contains('numero')){
+                    this.adicionarAoDisplay(el);
+                }
+
+                else if(el.classList.contains('Deletar')){
+                    this.limparDisplay(); 
+                }
+
+                else if(el.classList.contains('apagar')){
+                     this.apagarDigito();
+                }
+                
+                else if(el.classList.contains('igual')){
+                    this.calcular();
+                }
+                });
+        },
+
+        adicionarAoDisplay(el) {
+            this.display.value += el.innerText;
+        },
+
+        limparDisplay() {
+            this.display.value = '';
+        },
+
+        apagarDigito(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        calcular() {
+            this.display.value = eval(this.display.value);
+        }
 }
+}
+
+const inicializador = criaCalculadora();
+inicializador.iniciar();
