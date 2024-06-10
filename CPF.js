5dbabcb37e2ee52d4b848b71cf087a2d28e29630
edriel <edriel10gama10@gmail.com>
2024-06-09 22:32:21 -0300

    Objeto CPF

--- CPF.js
+++ CPF.js
@@ -1,8 +1,57 @@
 function Cpf(valor) {
-    Object.defineProperty(this, 'cpf', {
+    let value = valor;
+
+    const validarConstrutor =  (valor) => {if(this.verificarValor(valor))return value = valor;
+        value = false;
+        console.log('valor invalido');}
+    validarConstrutor(value);
+
+    Object.defineProperty(this, 'value', {
         enumerable: true,
         configurable: false,
-        get: () => Cpf,
-        set: (valor) => cpf = valor
+        get: () => value,
+        set: (valor) => {if(this.verificarValor(valor)) return value = valor;
+            value = false;
+            console.log('valor invalido');
+        }
     });
 }
+
+Cpf.prototype.verificarValor = function (valor = this.value) {
+    let valorLimpo = valor.replace(/\D+/g, '');
+    valorLimpo = Array.from(valorLimpo);
+    console.log(valorLimpo);
+    const valorChecagem = valorLimpo.slice(9, 11);
+    valorLimpo = valorLimpo.slice(0, -2);
+    console.log(valorChecagem);
+
+    if(!(Number(valorChecagem[0]) === this.uteis.pegaUltimoDoisDigitos(valorLimpo, 1))) return false;
+
+    valorLimpo.push(valorChecagem[1]);
+
+    if(!(Number(valorChecagem[1]) === this.uteis.pegaUltimoDoisDigitos(valorLimpo, 2))) return false;
+
+    return true;
+}
+
+Cpf.prototype.uteis = {
+
+    pegaUltimoDoisDigitos(array, indice_digito) {
+        let somaDigitos = array.reduce((acumulador, valor, indice) =>{
+        acumulador += ((9 + indice_digito) - indice) * Number(valor);
+        return acumulador;
+
+    }, 0);
+
+    console.log(somaDigitos);
+        return (11 - (somaDigitos % 11));
+
+    }
+}
+
+//Testando objeto
+//const CPF = new Cpf('115.921.984-22');
+//console.log(CPF.value);
+//console.log(CPF.verificarValor('123.123.123-11'));
+//CPF.value = '115.921.984-22';
+//console.log(CPF.verificarValor('115.921.984-22'));
