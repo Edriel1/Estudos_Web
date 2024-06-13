0db749fbbf87c6935768b435bcbeefa46dd416ea
edriel <edriel10gama10@gmail.com>
2024-06-13 02:01:11 -0300

    ajustes v1 Exer_8_validando_formulario

--- Exer_8_validando_formulario/JS/main.js
+++ Exer_8_validando_formulario/JS/main.js
@@ -1,37 +1,58 @@
-import {Cpf} from "../../CPF.js";
+//import {Cpf} from "../../CPF.js";
 
-function Formulario(nome, sobrenome, cpf, senha) {
-    Object.defineProperties(this, {
-        nome: {
-            enumerable: true,
-            configurable: false,
-            get: nome,
-            set: this.validaNome(nome)
-        },
-        sobrenome: {
-            enumerable: true,
-            configurable: false,
-            get: sobrenome,
-            set: this.validaSobrenome(sobrenome)
-        },
-        cpf: {
-            enumerable: true,
-            configurable: false,
-            get: cpf.value,
-            set: new Cpf(cpf)
-        },
-        senha: {
-            enumerable: true,
-            configurable: false,
-            get: senha,
-            set: validaSenha(senha)
+class ValidaFormulario{
+    constructor(){
+        this.formulario = document.querySelector('.formulario');
+
+        this.event();
+
+        
+    }
+    
+    event(){
+        this.formulario.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            this.validaCampos();
+        })
+    }
+
+    validaCampos(){
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            const anterior = campo.previousElementSibling.innerText;
+            let valido = true;
+
+            if(anterior === 'Nome:') valido = this.validaNome(campo);
+
         }
-    })
+    }
+
+    validaNome(campo) {
+        const valor = campo.value;
+        let valido = true;
+
+        if(!valor) {
+            valido = false;
+            this.criaErro(campo, 'O campo nome nâo pode estar vazio.');
+        }
+
+        if(valor < 3 || valor > 12){
+            valido = false;
+            this.criaErro(campo, 'O campo nome não esta dentro do limite de caracteres.');
+        }
+
+        if(!valor.match([/^a-zA-Z0-9+$/g])) {
+            valido = false;
+            this.criaErro(campo, 'O campo nome só pode ter letras e/ou numeros.');
+        }
+    }
+
+    criaErro(campo, texto){
+        const mensagem = document.createElement('div');
+        mensagem.classList.add('mensagem_erro');
+        mensagem.innerText = texto;
+        campo.insertAdjacentElement('afterend', mensagem); 
+    }
 }
 
-Formulario.prototype.validaNome = (nome) => {
-    if(!nome) return false;
-    if(typeof nome !== 'string') return false;
-    if(nome.length < 3 || nome.length > 12) return false;
-    if()
-}
+const formulario = new ValidaFormulario();
